a89a577f9fb176b948f384f7e39652926c0ce744
Minh Luc Van <minh.lucvan@ncc.asia>
2019-08-18 18:12:12 +0700

    adding fallback router

--- src/router.ts
+++ src/router.ts
@@ -66,5 +66,9 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '*',
+      redirect: 'home',
+    },
   ],
 });
