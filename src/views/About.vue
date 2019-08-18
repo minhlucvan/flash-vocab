080996b05131118358716c7174ef0558843db456
Minh Luc Van <minh.lucvan@ncc.asia>
2019-08-18 19:44:35 +0700

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,22 +4,32 @@
     <h1>Flash Vocab</h1>
     <p>Simple app for learning <b>vocabulary</b></p>
     <br>
+    <p>
+    <span>App developped by @minhlucvan</span>
     <br>
-    <p>App developped by @minhlucvan</p>
-    <p>Content provided by @myfriend</p>
+    <span>Content provided by @myfriend</span>
+    </p>
     <br>
     <p>
       If found an issue or have any feature request please contact me via email 
       <a href="mailto:luk.mink@gmail.com">luk.mink@gmail.com</a> 
     </p>
+    <br>
+    <br>
   </div>
 </template>
 
 <style scoped>
   .about {
-    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: space-around;
+    align-items: center;
+    height: 100%;
   }
   .img {
     max-width: 80%;
+    max-height: 40%;
   }
 </style>
