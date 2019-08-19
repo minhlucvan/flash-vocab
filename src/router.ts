225615809bc39f31a78e38031a2ac6092a288b04
Minh Luc Van <minh.lucvan@ncc.asia>
2019-08-19 09:20:19 +0700

    update routes

--- src/router.ts
+++ src/router.ts
@@ -61,14 +61,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
     },
+    { path: 'index.html', redirect: '/' },
     {
       path: '*',
-      redirect: 'home',
+      redirect: '/',
     },
   ],
 });
